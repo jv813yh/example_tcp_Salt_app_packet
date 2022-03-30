37818a1a73980b3a5f508d4d627b825810556359
jv813yh <56434164+jv813yh@users.noreply.github.com>
2022-03-30 16:36:05 +0200

    Add files via upload

---
+++ client00.c
@@ -0,0 +1,360 @@
+/**
+ * ===============================================
+ * client00.c   v.2.7
+ * 
+ * KEMT FEI TUKE, Diploma thesis
+ *
+ * The program is used for a simple demonstration 
+ * of the implementation of the Salt channel protocol 
+ * on TCP and working with the protocol.
+ *
+ * CLIENT: input from client -> CLI or
+ * creating / loading input file and creating
+ * salt handshake proccess and sending data
+ * from client with Salt channelv2 cryptographic
+ * to the server,the server (which also uses the 
+ * salt channel protocol) performs service and 
+ * sending the changing data back to the client:
+ *
+ * Encryption:
+ *      - Key exchange: X25519
+ *      - Encryption: XSalsa20 stream cipher
+ *      - Authentication: Poly1305 MAC
+ *
+ *  Signatures:
+ *      - Ed25519
+ *
+ *  Hashing:
+ *      - SHA512
+ *
+ * Deployment of Salt-Channelv2 cryptographic 
+ * protocol on TCP communication channel.
+ *
+ * Compileable on Windows with WinLibs standalone build 
+ * of GCC and MinGW-w64.
+ *
+ * Compileable on Linux with 
+ *
+ * For more details on salt channel see:
+ * https://github.com/assaabloy-ppi/salt-channel-c
+ *
+ * Author-Jozef Vendel  Create date- 24.12.2021 
+ * ===============================================
+ */
+
+/* ======== Includes ===================================== */
+
+/* Basic libraries for working in C. */
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+/**
+* Macro allows diagnostic information to be written 
+* to the standard error file.
+*/
+#include <assert.h>
+
+/* Libraries needed for networking work on Windows/Linux */
+#include "win_linux_sockets.h"
+
+/* ===== Salt-channel libraries ===== */
+
+#include "salt.h"
+#include "salt_io.h"
+
+/* 
+ * Created functions for implementing 
+ * Salt channel protocol on TCP/IP  
+ */
+#include "example_tcp_salt.h"
+
+int main(int argc, char *argv[]) 
+{
+    /* Checking the correct program parameters */
+    if (argc < 3) {
+        fprintf(stderr, "usage: tcp_client hostname port\n");
+        return 1;
+    }
+
+    /* Initializing Winscok - Win32 apps , ONLY FOR WINDOWS !!! */
+#if defined(_WIN32)
+    WSADATA d;
+    if (WSAStartup(MAKEWORD(2, 2), &d)) {
+        fprintf(stderr, "Failed to initialize.\n");
+        return 1;
+    }
+#endif
+
+    /* Basic description of the program */
+    printf("\nDeployment of Salt-Channelv2 cryptographic\n" 
+        "protocol on TCP communication channel.\n");
+    printf("\nCLIENT: input from client -> CLI or\n"
+        "creating / loading input file and creating\n"
+        "salt handshake proccess and sending data\n"
+        "from client with Salt channelv2 cryptographic\n"
+        "to the server,the server (which also uses the\n" 
+        "salt channel protocol) performs service and\n" 
+        "sending the changing data back to the client\n\n");
+
+/* ========  Variables & arrays ======== */
+
+    /* Own file name */
+    char own_file[AUXILIARY_FIELD_SIZE];     
+
+    /* 
+     * test: you can choose whether you want to 
+     * enter your own data or use a test program
+     *
+     * repeat: for the decision on the continue/finish 
+     * of the program
+     *
+     * print_out: statement of received data
+     */
+    int repeat, test, print_out = 0, free_input = 0,    
+
+    /* 
+     * Variables for auxiliary calculations when 
+     * working with blocks for protocol (size of buffer)
+     */
+        count_of_read_blocks = 0,
+        count_of_write_blocks = 0;
+
+    /**
+     * tx_buffer -> encrypted data
+     * input -> loading input file 
+     * rx_buffer -> received/decrypted data
+     */ 
+    uint8_t *tx_buffer, *input, *rx_buffer;
+
+    /* The size of the transferred file. */    
+    uint32_t file_size;
+
+    /* Client socket for TCP */
+    SOCKET socket_peer;
+
+/* ====  Variables for working with salt-channel protocol ===== */
+
+    /* Salt channel structure */
+    salt_channel_t client_channel;
+
+    /* 
+     * Verification of return values during working with protocol  
+     *
+     * typedef enum
+     * which can obtain values:
+     * SALT_SUCCESS, SALT_PENDING, SALT_ERROR            
+     */
+    salt_ret_t ret_hndshk = SALT_ERROR, ret_msg = SALT_ERROR;
+
+    /**
+     * Structure used (protocol) for easier 
+     * creating / reading / working with data 
+     */
+    salt_msg_t msg_out, msg_in;
+
+/* ==== Creating client socket and TCP connection ==== */
+
+    /* 
+     * It will create a socket with which the client will 
+     * connect to the server
+     */
+    socket_peer = create_socket_and_connect(argv[1], argv[2]);
+
+    /* Connection control */
+    if (ISVALIDSOCKET(socket_peer)) printf("\nConnection to the server :)\n");
+    else printf("\nError connecting to server :(\n");
+
+    /*
+     * TCP connection was successfully established in the 
+     * client-server architecture.
+     * 
+     * Now, We are implementing salt channel v2 
+     * and We will try a salt handshake with the server
+     */
+    printf("Connected.\n");
+    printf("TCP was successfully performed...\n\nCreating Salt Hanshake...\n");
+
+/* ========  Salt-channel implementation and Salt Handshake  ======== */
+
+    ret_hndshk = salt_impl_and_hndshk(&client_channel,
+                                    my_write,
+                                    my_read,
+                                    &socket_peer,
+                                    &my_time,
+                                    TRESHOLD);
+    /* Implementation and Handshake verification */
+    assert(ret_hndshk == SALT_SUCCESS);
+
+/* ======= Sending/receiving data in cycles ==================== */
+/* ======= Encryption/decryption data ========================== */
+    while(ret_hndshk == SALT_SUCCESS)
+    {
+        free_input = 0;
+        printf("\n\nDo you want to use a random text file to test the application\n" 
+            "or you want to use the application according to your needs\n"
+            "Press number 0 or 1,\n'1' -> to test the application\n"
+            "'0' -> own data\n");
+        if (EOF == (scanf("%d", &test)))
+        {
+            printf("Bad choise :(\n");
+            return -1;
+        }
+
+/* ========  Loading input data (file)  ================= */
+        if (test)
+        {  
+            printf("\n\nYou have decided to create a test file to test the application.\n"
+                "Follow this instructions:\n"
+                "Please enter name of file with suffix, example: example.txt \n");
+            scanf("%s", own_file);
+
+            /* Random generate file and loading input data */
+            input = loading_file(own_file, &file_size, 0); 
+
+            printf("\nFile size is: %u\n", file_size);
+
+            //Check if the memory has been successfully
+            //allocated by malloc or not
+            if (input == NULL)
+            {
+                printf("Memory not allocated in loading file\n");
+                exit(0);
+            }
+            free_input++;
+        } else 
+        {   
+/* =======  Loading data from client (CLI) === */
+            uint8_t input_cli[AUXILIARY_FIELD_SIZE];
+            input = input_cli;
+
+            printf("To send data, enter text followed by enter.\n");
+            /* Cleaning buffer from enter */
+            while(getchar() != '\n')
+                ;
+
+            /* Receipt of input data from the client (CLI) */
+           if (!fgets((char *)input, AUXILIARY_FIELD_SIZE, stdin)) break;
+            file_size = strlen((const char *) input);
+
+            printf("\nSize of data is: %u\n\n", file_size);
+        }
+
+        /* 
+         * Calculating the number of blocks and the additional memory 
+         * required to work with the data for protocol
+         */
+
+        count_of_write_blocks = calculated_count_of_blocks(file_size,
+                                                           BLOCK_SIZE,
+                                                           SALT_WRITE_OVRHD_SIZE);
+        count_of_write_blocks = SALT_WRITE_OVRHD_SIZE + (count_of_write_blocks * 2);
+        if (count_of_write_blocks <= 0)
+        {
+            printf("Error calculating memory size for blocks\n");
+            return -1;
+        }
+        count_of_read_blocks = calculated_count_of_blocks(file_size,
+                                                           BLOCK_SIZE,
+                                                           SALT_READ_OVRHD_SIZE);
+        count_of_read_blocks = SALT_READ_OVRHD_SIZE + (count_of_write_blocks * 2);
+        if (count_of_write_blocks <= 0)
+        {
+            printf("Error calculating memory size for blocks\n");
+            return -1;
+        }
+
+        /* Allocates the requested memory and returns a pointer to it */
+        tx_buffer = (uint8_t *) malloc(file_size + count_of_write_blocks);
+        rx_buffer = (uint8_t *) malloc(file_size + count_of_read_blocks);
+
+        //Check if the memory has been successfully
+        //allocated by malloc or not
+        if((tx_buffer == NULL) || (rx_buffer == NULL))
+        {
+            printf("Memory not allocated.\n");
+            exit(0);   
+        }
+
+/* ======= Sending/receiving data ==================== */
+/* ======= Encryption/decryption data ================ */
+
+        while(ret_hndshk == SALT_SUCCESS) 
+        { 
+            printf("Do you want to list the received data?\n"
+                "If yes, press -> '1'\n"
+                "Or no, press -> '0'\n");
+            if (scanf("%d", &print_out) == EOF)
+            {
+                printf("Error: Do you want to list the received data?\n");
+                return -1;
+            }
+
+            /* Filling a block of memory with a particular value */
+            memset(tx_buffer, 0, file_size + count_of_write_blocks);
+            memset(rx_buffer, 0, file_size + count_of_read_blocks);
+
+            /* 
+             * Created a function for preparing buffer and data,
+             * then encrypting and sending data in the salt channel protocol
+             */
+            ret_msg = salt_encrypt_and_send(&client_channel,
+                                        tx_buffer,
+                                        file_size + count_of_write_blocks,
+                                        file_size,
+                                        BLOCK_SIZE,
+                                        input,
+                                        &msg_out);
+            /* Verification */
+            assert(ret_msg == SALT_SUCCESS);
+
+            ret_msg = SALT_ERROR;
+            /* 
+             * Created function for data reception, decryption, 
+             * verification and reading in the salt channel protocol
+             */
+            ret_msg = salt_read_and_decrypt(&client_channel,
+                                            rx_buffer,
+                                            file_size + count_of_read_blocks,
+                                            &msg_in,
+                                            NULL,
+                                            NULL,
+                                            print_out);
+            assert(ret_msg == SALT_SUCCESS);
+
+            /* End of sending and receiving data, encryption and decryption */ 
+            printf("\nDo you want to finish?\nPress number, '1'-> to quit\n"
+                "Press '0' -> to continue\n");
+            scanf("%d", &repeat);
+
+            if(free_input) 
+                free(input);
+
+            free(tx_buffer);
+            free(rx_buffer);
+
+            break;
+        }
+        if(repeat) break;
+
+        /* If everything is OK, the cycle continues */
+        ret_hndshk = ret_msg;
+    }
+
+/* ========  End of application  ========================= */
+
+    printf("\nClosing connection...\n");
+    printf("Closing socket...\n");
+    /* Socket closure */
+    CLOSESOCKET(socket_peer);
+
+    /* Cleanning Winsock, ONLY FOR WINDOWS !!! */
+#if defined(_WIN32)
+    WSACleanup();
+#endif
+
+    printf("Finished.\n");
+
+    return 0;
+}
---
+++ server00.c
@@ -0,0 +1,311 @@
+/**
+ * ===============================================
+ * server00.c   v.2.5
+ * 
+ * KEMT FEI TUKE, Diploma thesis
+ * 
+ * The program is used for a simple demonstration 
+ * of the implementation of the Salt channel protocol 
+ * on TCP and working with the protocol.
+ *
+ * SERVER: The server establishes a TCP connection, 
+ * the client connects, and a Salt handshake occurs 
+ * between the partners. The server provides the service
+ * using the toupper(), where it converts all data received
+ * from the client by the toupper() function and sends the 
+ * changed data back to the client. Cryptographic security 
+ * is provided by the salt channel protocol.
+ *
+ * Encryption:
+ *      - Key exchange: X25519
+ *      - Encryption: XSalsa20 stream cipher
+ *      - Authentication: Poly1305 MAC
+ *
+ *  Signatures:
+ *      - Ed25519
+ *
+ *  Hashing:
+ *      - SHA512
+ *
+ * Deployment of Salt-Channelv2 cryptographic 
+ * protocol on TCP communication channel.
+ *
+ * Compileable on Windows with WinLibs standalone build 
+ * of GCC and MinGW-w64.
+ *
+ * Compileable on Linux with 
+ *
+ * For more details on salt channel see:
+ * https://github.com/assaabloy-ppi/salt-channel-c
+ *
+ * Author-Jozef Vendel  Create date- 20.12.2021 
+ * ===============================================
+ */
+
+/* ======== Includes ===================================== */
+
+/* Basic libraries for working in C. */
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <ctype.h>
+
+/**
+* Macro allows diagnostic information to be written 
+* to the standard error file.
+*/
+#include <assert.h>
+
+/* Libraries needed for networking work on Windows/Linux */
+#include "win_linux_sockets.h"
+
+/* ===== Salt-channel libraries ===== */
+#include "salt.h"
+#include "salt_io.h"
+
+/* 
+ * Created functions for implementing 
+ * Salt channel protocol on TCP/IP  
+ */
+#include "example_tcp_salt.h"
+
+/* ========== Ready sk_sec_key for server ======== */
+#include "server_sk_key.h"
+
+/* ======= Local Macro Definitions ======================= */
+
+/* Maximum size of data transmitted */
+#define MAX_SIZE            UINT16_MAX * 30
+
+int main(int argc, char *argv[]) 
+{ 
+    /* Checking the correct program parameters */
+    if (argc < 3) {
+        fprintf(stderr, "usage: tcp_server ip_host port\n");
+        return 1;
+    }
+
+    /* Initializing Winscok - Win32 apps , ONLY FOR WINDOWS !!! */
+#if defined(_WIN32)
+    WSADATA d;
+    if (WSAStartup(MAKEWORD(2, 2), &d)) {
+        fprintf(stderr, "Failed to initialize.\n");
+        return 1;
+    }
+#endif
+
+    /* Basic description of the program */
+    printf("\nDeployment of Salt-Channelv2 cryptographic\n" 
+        "protocol on TCP communication channel.\n");
+    printf("\nSERVER: The server establishes a TCP connection\n" 
+        "the client connects, and a Salt handshake occurs\n" 
+        "between the partners. The server provides the service\n"
+        "using the toupper(), where it converts all data received\n"
+        "from the client by the toupper() function and sends the\n" 
+        "changed data back to the client. Cryptographic security\n" 
+        "is provided by the salt channel protocol.\n\n\n");
+
+/* ========  Variables ======== */
+    /* 
+     * tx_buffer: receive decrypted modified data, 
+     * which is encrypted and sent back to the client
+     * rrx_buffer: receive encrypted data from the client
+     * copy_buffer: receive decrypted data from the client
+     */
+    uint8_t *tx_buffer, *rrx_buffer, *copy_buffer;
+
+    /* The size of the decrypted data */
+    uint32_t decrypt_size;
+
+    /* 
+     * client socket: is used to communicate with the client 
+     * after a successful tcp handshake   
+     *
+     * socket_listen: socket for server for listen
+     */
+    SOCKET socket_client, socket_listen;
+
+/* ====  Variables for working with salt-channel protocol ==== */
+
+    /* Salt channel structure */
+    salt_channel_t server_channel;
+
+    /* Initiates to add information about supported protocols to host */
+    salt_protocols_t protocols;
+
+    /**
+     * Structure used (protocol) for easier 
+     * creating / reading / working with data 
+     */
+    salt_msg_t msg_out, msg_in;
+
+    /* 
+     * Verification of return values during working with protocol  
+     *
+     * typedef enum
+     * which can obtain values:
+     * SALT_SUCCESS, SALT_PENDING, SALT_ERROR            
+     */
+    salt_ret_t ret_hndshk = SALT_ERROR, ret_msg = SALT_ERROR;
+
+/* ==== Creating socket and TCP connection ==== */
+    
+    /* 
+     * Creates a socket (return this socket) with which it waits 
+     * for a connection from the client   
+     */
+    socket_listen = create_socket_and_listen(argv[1], argv[2]);
+
+    printf("Waiting for connections...\n");
+
+    /* Creating TCP connection with client */
+    struct sockaddr_storage client_address;
+    socklen_t client_len = sizeof(client_address);
+    socket_client = accept(socket_listen,
+                            (struct sockaddr*) &client_address,
+                            &client_len);
+
+    /* Verification of the correct establishment of the connection */
+    if (!ISVALIDSOCKET(socket_client)) 
+    {
+        fprintf(stderr, "accept() failed. (%d)\n",
+                GETSOCKETERRNO());
+        return 1;
+    } else 
+    {
+        /* Listing the ip address of the client */
+        char address_buffer[AUXILIARY_FIELD_SIZE];
+        int return_getnameinfo;
+        return_getnameinfo = getnameinfo((struct sockaddr*)&client_address,
+                client_len, address_buffer, sizeof(address_buffer), 0, 0,
+                NI_NUMERICHOST);
+
+        if (return_getnameinfo)
+        {
+            fprintf(stderr, "getnameinfo() failed. (%d)\n", GETSOCKETERRNO());
+            return 1;
+        }
+        /*
+         * TCP connection was successfully established in the 
+         * client-server architecture, 
+         * it is possible to access the data exchange.
+         */
+        printf("\nNew connection from %s\n\n", address_buffer);
+    }
+
+    /*
+     * TCP connection was successfully established in the 
+     * client-server architecture.
+     * 
+     * Now, We are implementing salt channelv2 
+     * and We will try a Salt Handshake with the server
+     * and exchange data.
+     */
+
+/* ======== Salt-channel implementation & Handshake ======== */
+
+    /*
+     * Created a function (server) for deploying the Salt channel and 
+     * creating a Salt Handshake on TCP/IP using a socket
+     */
+    ret_hndshk = salt_impl_and_hndshk_server(&server_channel,
+                                             &protocols,
+                                             my_write,
+                                             my_read,
+                                             &socket_client,
+                                             &my_time,
+                                             host_sk_sec,
+                                             TRESHOLD);
+    /* Implementation and Handshake verification */
+    assert(ret_hndshk == SALT_SUCCESS);
+
+    /* Allocates the requested memory and returns a pointer to it */
+
+    /* Buffer for data before send and encrypted to clien*/
+    tx_buffer = (uint8_t *) malloc(MAX_SIZE);
+    /* Buffer for receiving encrypted message from client */
+    rrx_buffer = (uint8_t *) malloc(MAX_SIZE);
+    /* Buffer for decrypted and modifying data from client */
+    copy_buffer = (uint8_t *) malloc(MAX_SIZE);
+
+    if((tx_buffer == NULL) || (rrx_buffer == NULL) || (copy_buffer == NULL))
+    {
+        printf("Memory not allocated.\n");
+        exit(0);   
+    }
+
+/* ======= Sending/receiving data in cycles ==================== */
+/* ======= Encryption/decryption data ========================== */
+
+    while(ret_hndshk == SALT_SUCCESS) 
+    {
+        decrypt_size = 0;
+
+        /* Filling a block of memory with a particular value */
+        memset(rrx_buffer, 0, MAX_SIZE);
+        memset(tx_buffer, 0, MAX_SIZE);
+
+        /*    
+         * Created function (server) for data reception, 
+         * authentication and decryption with protocol   
+         */
+        ret_msg = salt_read_and_decrypt(&server_channel,
+                                        rrx_buffer,
+                                        MAX_SIZE,
+                                        &msg_in,
+                                        copy_buffer,
+                                        &decrypt_size,
+                                        0);
+        /* Verification */
+        assert(ret_msg == SALT_SUCCESS);
+
+        if (ret_msg == SALT_SUCCESS)
+        {   
+            /**
+             * The server provides the toupper() service, 
+             * converting characters to uppercase
+             */
+            for (uint32_t k = 0; k < decrypt_size; k++) 
+                copy_buffer[k] = toupper(copy_buffer[k]);
+
+            ret_msg = SALT_ERROR;
+            /*   
+             * Created function for buffer preparation, also data, 
+             * encryption and sending using protocol 
+             */
+            ret_msg = salt_encrypt_and_send(&server_channel,
+                                            tx_buffer,
+                                            MAX_SIZE,
+                                            decrypt_size,
+                                            BLOCK_SIZE,
+                                            copy_buffer,
+                                            &msg_out);
+            /* Verification */
+            assert(ret_msg == SALT_SUCCESS);
+        }
+
+        /* If everything is OK, the cycle continues */
+        ret_hndshk = ret_msg;
+    } 
+
+/* ========  End of application  ========================= */
+
+    printf("\nClosing connection...\n");
+    /* Socket closure */
+    printf("Closing sockets...\n");
+    CLOSESOCKET(socket_client);
+    CLOSESOCKET(socket_listen);
+
+    free(tx_buffer);
+    free(copy_buffer);
+    free(rrx_buffer);
+
+    /* Cleanning Winsock, ONLY FOR WINDOWS !!! */
+#if defined(_WIN32)
+    WSACleanup();
+#endif
+
+    printf("Finished.\n");
+
+    return 0;
+}
